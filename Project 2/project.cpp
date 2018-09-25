12e76440e431a64894e1bca7a81780bd498b121d
Audun <auduntre@student.matnat.uio.no>
2018-09-26 01:37:56 +0200

    oops

---
+++ Project 2/project.cpp
@@ -0,0 +1,10 @@
+#include <armadillo>
+#include <cmath>
+
+#include "jacobi.h"
+
+arma::uword maxoffdiag (arma::mat X)
+{
+    // Set diag to zero and find index of extreme value in matrix
+    return arma::abs(arma::trimatu(X)).index_max();
+}
