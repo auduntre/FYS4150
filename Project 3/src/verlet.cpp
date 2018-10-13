22b5dcf5e5591a958cc8141b34781a00c6ac64aa
Audun <auduntre@student.matnat.uio.no>
2018-10-13 20:17:46 +0200

    removing unecessary class-methods

--- Project 3/src/verlet.cpp
+++ Project 3/src/verlet.cpp
@@ -27,30 +27,9 @@ void Verlet::integrateOneStep (SolarSystem &system)
 }
 
 
-void Verlet::initialStep(class SolarSystem &system)
-{
-    system.calculateForcesAndEnergy();
-    
-    for (CelestialBody &body : system.bodies()) {
-        body.position = body.position + body.velocity * this->h()
-                      + 0.5 * (body.force / body.mass) * this->dtdt;
-    }
-    
-    system.calculateForcesAndEnergy();
-
-    for (CelestialBody &body : system.bodies()) {
-        body.velocity = body.velocity + 0.5 * this->h()
-                      * (body.past_force / body.mass 
-                      + body.force / body.mass);
-    }
-}
-
-
 void Verlet::integrateNtimes(class SolarSystem &system, int nTimes)
 {
-    this->initialStep(system);
-
-    for (int i = 1; i < nTimes; i++) {
+    for (int i = 0; i < nTimes; i++) {
         this->integrateOneStep(system);
     }
 }
