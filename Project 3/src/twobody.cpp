78f7266316c42a71c33c46344ff55653d77b7441
Audun <auduntre@student.matnat.uio.no>
2018-10-24 22:59:05 +0200

    notebook

--- Project 3/src/twobody.cpp
+++ Project 3/src/twobody.cpp
@@ -23,12 +23,20 @@ int main (int argc, char **argv)
     arma::vec3 vel = {0, -1.8849555921538758e-05, 0};
     double mass = 1.0;
 
+    if (argc >= 4) {
+        vel(1) = - std::atof(argv[3]) * 3e-6;
+    }
+    
     CelestialBody &sun = sol.createCelestialBody(pos, vel, mass);
 
     pos = {1, 0, 0};
     vel = {0, 2 * M_PI, 0};
     mass  = 3e-6;
 
+    if (argc >= 4) {
+        vel(1) = std::atof(argv[3]);
+    }
+    
     CelestialBody &earth = sol.createCelestialBody(pos, vel, mass);
 
     std::vector<CelestialBody> &bodies = sol.bodies();
@@ -43,12 +51,11 @@ int main (int argc, char **argv)
     double dt = endTime / nTimesteps;
 
     std::cout << "################# CALCULATE #################" << std::endl;
-    Euler integrator(dt);
+    Verlet integrator(dt);
     integrator.initialStep(sol);
     
     arma::mat posMat(3, nTimesteps, arma::fill::zeros); 
     posMat.col(0) = earth.position;
-
 
     for (int i = 1; i < nTimesteps; i++) {
         integrator.integrateOneStep(sol);
@@ -56,8 +63,14 @@ int main (int argc, char **argv)
         posMat.col(i) = earth.position;
     }
 
-    posMat.save("../positions/positions" + std::to_string(nTimesteps) + ".txt",
-                arma::raw_ascii);
+    if (argc >= 4) {
+        std::string velstr(argv[3]);
+        posMat.save("../positions/escape" + velstr + ".txt",
+                     arma::raw_ascii);
+    } else {
+        posMat.save("../positions/positions" + std::to_string(nTimesteps) + ".txt",
+                    arma::raw_ascii);
+    }
 
     bodyPrint(bodies);
     
--- Project 3/src/twobodybeta.cpp
+++ Project 3/src/twobodybeta.cpp
@@ -1,7 +1,7 @@
 #include <cstdlib>
 #include <string>
 
-#include "solarsystem.h"
+#include "betasolarsystem.h"
 #include "euler.h"
 #include "verlet.h"
 
@@ -10,7 +10,10 @@ int main(int argc, char **argv)
 {
     void betaRun (double beta);
 
-    for (double beta: betas) {  
+    arma::vec betas = {2.00, 2.50, 2.75, 2.90, 2.95, 3.00};
+    
+    for (double beta: betas) {
+        betaRun(beta);
     }
 
     return 0;
@@ -19,22 +22,37 @@ int main(int argc, char **argv)
 
 void betaRun(double beta)
 {
-    arma::vec betas = {2.00, 2.50, 2.75, 2.90, 2.95, 3.00};
 
-    double mass_earth = 3.0e-6;
-    arma::vec3 pos_earth = {1.0, 0.0, 0.0};
-    arma::vec3 vel_earth = {0.0, 6.28, 0.0};
-    
-    CelestialBody &sun = sol.createCelestialBody(pos, vel, mass);
-    
+    BetaSolarSystem sol(beta);
+
     double mass_sun = 1.0;
-    arma::vec3 pos_sun = {0.0, 0.0, 0.0};
-    arma::vec3 vel_sun = - vel_earth * mass_earth / mass_sun;
+    arma::vec3 pos = {0.0, 0.0, 0.0};
+    arma::vec3 vel = {0.0, -1.8849555921538758e-05, 0.0};
 
-    CelestialBody &earth = sol.createCelestialBody(pos, vel, mass);
-
-    double nTimesteps = 5000000;
+    CelestialBody &sun = sol.createCelestialBody(pos, vel, mass_sun);
+    
+    double mass_earth = 3.0e-6;
+    pos = {1.0, 0.0, 0.0};
+    vel = {0.0, 6.28, 0.0};
+    
+    CelestialBody &earth = sol.createCelestialBody(pos, vel, mass_earth);
+    
+    int nTimesteps = 500000;
     double endTime = 5.0;
+    double dt = endTime / nTimesteps;
 
+    Verlet integrator(dt);
+
+    arma::mat posMat(3, nTimesteps, arma::fill::zeros);
+    posMat.col(0) = earth.position;
+    
+    for (int i = 1; i < nTimesteps; i++) {
+        integrator.integrateOneStep(sol);
+        posMat.col(i) = earth.position;
+    }
+
+    posMat.save("../positions/beta" + std::to_string(beta) + ".txt",
+                arma::raw_ascii);
+    
     return;
 }
